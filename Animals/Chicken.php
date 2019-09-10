2cad4b038a47960ff04f29256271cf2ff8beed40
Cuong Nguyen <cuongnmph04158@gmail.com>
2019-09-10 10:34:35 +0700

    Up code

--- Animals/Chicken.php
+++ Animals/Chicken.php
@@ -1,10 +1,14 @@
 <?php
 require_once dirname(__FILE__).'/../AbstractClass/Animal.php';
-
-class Chicken extends Animal
+include_once(dirname( __FILE__ ) . '/../InterfaceClass/Edible.php');
+class Chicken extends Animal implements Edible
 {
    public function makeSound()
    {
       return "Chicker: Cluck - Cluck";
+   }
+   public function howtoEat()
+   {
+      return "Could be fried";
    }
 }
--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php
 include ("Animals/Chicken.php");
 include ("Animals/Tiger.php");
-
+include ("Fruits/Apple.php");
+include ("Fruits/Orange.php");
 
 echo ("---------Animal------------<br>");
 
@@ -10,4 +11,17 @@ $animals[1] = new Tiger();
 
 foreach ($animals as $animal){
    echo $animal->makeSound()."<br>";
+
+   if ($animal instanceof Chicken){
+      echo $animal->howtoEat()."<br>";
+   }
+}
+
+echo ('--------- Fruits------------<br>');
+
+$fruits[0] = new Apple();
+$fruits[1] = new Orange();
+
+foreach ($fruits as $fruit) {
+   echo $fruit->howToEat() . '<br>';
 }
